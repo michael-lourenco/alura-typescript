372ae39f963ce3ec44b6277f2475efe0c5db2cb9
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-01 10:32:14 -0300

    Negociação, modelagem e regras

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-alert('oi');
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 1, 200);
+console.log(negociacao);
+negociacao.quantidade = 1000;
+console.log(negociacao);

c27262aa373c9de59cc97e0cbf01b56bc6575b9a
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-01 10:38:55 -0300

    Finalização do modelo

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 1, 200);
-console.log(negociacao);
-negociacao.quantidade = 1000;
-console.log(negociacao);
+const negociacao = new Negociacao(new Date(), 10, 200);
+console.log(negociacao.volume);
